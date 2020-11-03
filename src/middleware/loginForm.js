761044d41643edad91da212e6389956ec6a11fde
renautech <renaud.second@outlook.fr>
2020-11-03 09:59:58 +0100

    profil refresh between two users login / 'Mon jardin' and 'Mon profil' removed from nav bar when user is not logged in

--- src/middleware/loginForm.js
+++ src/middleware/loginForm.js
@@ -7,16 +7,20 @@ import {
   loginError,
   loggedOut,
 } from 'src/actions/loginForm';
+import { enableLoading } from 'src/actions/profile';
 
 const loginForm = (store) => (next) => (action) => {
   switch (action.type) {
     case LOGIN: {
       const { loginForm: { email, password } } = store.getState();
+      //axios.post('http://localhost:5555/v1/signin', { email, password }, { withCredentials: true })
       axios.post('http://3.93.151.102:5555/v1/signin', { email, password }, { withCredentials: true })
         .then((res) => {
           if (res.data.state === true) {
             // console.log('logged');
             store.dispatch(isLogged());
+            // Authorize fetching new profile data at next profile component rendering
+            store.dispatch(enableLoading());
           }
           else {
             console.log(res.data.message);
